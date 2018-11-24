a12835240cd1bf8312484e9f0933e197525a0c5c
zhifeng ou <zhifengou@hotmail.com>
2018-11-24 22:35:04 +0000

    Done.

--- src/Timer.js
+++ src/Timer.js
@@ -11,7 +11,16 @@ class Timer extends Component {
     }
   }
 
+shouldComponentUpdate(nextProps, nextState) {
+      if (this.state.time === nextState.time) {
+        return false
+      }
+      return true
+    }
 
+  componentDidUpdate() {
+   this.timer.current.style.color = '#'+Math.floor(Math.random()*16777215).toString(16)
+ }
 
   //Your code here
 
